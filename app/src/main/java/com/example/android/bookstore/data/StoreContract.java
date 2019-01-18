9975d6c12cac4f3512815bd60d47098064e3160a
Sau1o <coelho.saulo@gmail.com>
2019-01-18 15:29:09 -0200

    add StoreProvider

--- app/src/main/java/com/example/android/bookstore/data/StoreContract.java
+++ app/src/main/java/com/example/android/bookstore/data/StoreContract.java
@@ -1,14 +1,28 @@
 package com.example.android.bookstore.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class StoreContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.books";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_BOOKS = "books";
 
     private StoreContract(){
 
     }
 
     public static final class BookEntry implements BaseColumns{
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
 
         public final static String TABLE_NAME = "books";
 
@@ -18,7 +32,5 @@ public final class StoreContract {
         public final static String COLUMN_BOOK_QUANTITY = "quantity";
         public final static String COLUMN_BOOK_SUPPLIER_NAME = "supplier_name";
         public final static String COLUMN_BOOK_SUPPLIER_PHONE = "supllier_phone";
-
-
     }
 }
